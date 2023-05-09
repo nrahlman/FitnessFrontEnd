620f011a2c95b3acb00918354030985fb593cbaf
jwwalls <jwwalls@users.noreply.github.com>
2023-05-09 12:21:05 -0500

    cassieupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './App.css'
 import NavBar from './Components/NavBar'
 import RRoutes from './Components/Routes'
+import React, {useState} from 'react';
+
+
 
 
 function App() {
-  
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState({});
+
 
   return (
     <div className="App">
-      <NavBar/>
-      <RRoutes/>
+      <NavBar user={user}/>
+      <RRoutes token={token} setToken={setToken} setUser={setUser}/>
     </div>
   )
 }
+
 
 export default App
