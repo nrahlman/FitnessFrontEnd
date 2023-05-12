e0ca0d83032f859508ed60eb78b00959cba9cab0
jwwalls <jwwalls@users.noreply.github.com>
2023-05-12 15:24:44 -0500

    myroutines

--- src/Pages/MyRoutines.jsx
+++ src/Pages/MyRoutines.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { fetchMe, fetchMyRoutines } from '../API/user';
-import '../Styles/myRoutines.css'
-
-function MyRoutines({user, setUser}) {
+import React, { useState, useEffect } from "react";
+import { fetchMe, fetchMyRoutines } from "../API/user";
+import { GetRoutines, links, DisplayActivities } from "../API/activities";
+import { addActivityToRoutine } from "../API/routines";
+function MyRoutines({ user, setUser }) {
   const [routines, setRoutines] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [selectedActivity, setSelectedActivity] = useState(0);
+
+  const [count, setCount] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       fetchMe(token)
         .then((userData) => {
@@ -20,34 +25,115 @@ function MyRoutines({user, setUser}) {
           console.error(err);
         });
     }
+
+    async function fetchActivities() {
+      try {
+        const result = await DisplayActivities();
+        setActivities(result);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchActivities();
   }, []);
+
+  const handleAddActivity = async (routineId, activityId) => {
+    try {
+      await addActivityToRoutine(routineId, selectedActivity, count, duration); // updated argument
+      const updatedRoutines = await fetchMyRoutines(
+        localStorage.getItem("token"),
+        user.username
+      );
+      setRoutines(updatedRoutines);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   if (!user.username) {
     return (
-        <div className='myRoutines'>
-    <h1 className='header'>My Routines</h1>
-    <p className='message'>Please log in to view your routines</p>
-    </div>
-    )
+      <div className="my-routines">
+        <h1 className="header">My Routines</h1>
+        <p className="message">Please log in to view your routines</p>
+      </div>
+    );
   }
 
   return (
-    <div className='myRoutines'>
-      <h1 className='header'>My Routines</h1>
-      <p className='message'>Welcome back, {user.username}!</p>
-      {routines.length > 0 ? (
-        <ul>
-          {routines.map((routine) => (
-            <li key={routine.id}>
-              <h2>{routine.name}</h2>
-              <p>{routine.description}</p>
-              {/* additional routine details here */}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className='message2'>You don't have any routines yet</p>
-      )}
+    <div className="my-routines">
+      <h1 className="header">My Routines</h1>
+      <p className="message">Welcome back, {user.username}!</p>
+      {routines.map((routine) => {
+        return (
+          <div className="routine" key={routine.id}>
+            <div className="routine-header">
+              <h2>
+                {routine.name}{" "}
+                <span className="routine-author">
+                  by: {routine.creatorName}
+                </span>
+              </h2>
+              <button onClick={() => handleAddActivity(routine.id)}>
+                Add New Activity
+              </button>
+            </div>
+            <ul className="activity-list">
+              {routine.activities.map((activity) => {
+                return (
+                  <li key={activity.id}>
+                    <h3>{activity.name}</h3>
+                    <p>{activity.description}</p>
+                    <p>Count: {activity.count}</p>
+                    <p>Duration: {activity.duration}</p>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="select-activity">
+              <label htmlFor="activity-select">Select Activity:</label>
+              <select
+                id="activity-select"
+                value={selectedActivity.id}
+                onChange={(event) => setSelectedActivity(event.target.value)}
+              >
+                <option value="">--Please choose an activity--</option>
+                {activities.map((activity) => (
+                  <option key={activity.id} value={activity.id}>
+                    {activity.name}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor="count">Count:</label>
+              <input
+                type="number"
+                id="count"
+                value={count}
+                onChange={(event) => setCount(event.target.value)}
+              />
+              <label htmlFor="duration">Duration (in minutes):</label>
+              <input
+                type="number"
+                id="duration"
+                value={duration}
+                onChange={(event) => setDuration(event.target.value)}
+              />
+              <button
+                onClick={() =>
+                  handleAddActivity(
+                    routine.id,
+                    selectedActivity.id,
+                    count,
+                    duration
+                  )
+                }
+              >
+                Add Activity
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
